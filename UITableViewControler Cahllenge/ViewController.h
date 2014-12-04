6ae092d0d538e8c1348dcb4416ef827219f95df9
ranek <ranek@design23.pacificwebworks.com>
2014-12-04 14:50:43 -0700

    Initial Commit

---
+++ UITableViewControler Cahllenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewControler Cahllenge
+//
+//  Created by ranek on 12/4/14.
+//  Copyright (c) 2014 B109. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewControler Cahllenge/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITableViewControler Cahllenge
+//
+//  Created by ranek on 12/4/14.
+//  Copyright (c) 2014 B109. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
